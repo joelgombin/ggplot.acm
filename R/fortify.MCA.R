ef5def04f8e3f08dddbae32f800fa5df2c0df361
Joel Gombin <joel.gombin@gmail.com>
2012-11-07 17:42:03 +0100

    corrigé de vieux restes d'acm en model

--- R/fortify.MCA.R
+++ R/fortify.MCA.R
@@ -1,3 +1,19 @@
+#' Prepares the data for \code{ggplot.model}. 
+#'
+#' @param model an model of class MCA
+#' @param data not used.
+#' @param ... other parameters to be passed.
+#' @return a dataframe suitable for \code{autoplot.MCA}.
+#' @keywords MCA, ggplot2, graphics
+#' @seealso \code{\link{autoplot.MCA}} 
+#' @export
+#' @examples
+#' library(FactoMineR)
+#' data(tea)
+#' tea.mca <- MCA(tea[,1:18], graph=FALSE)
+#' autoplot(tea.mca)
+
+
 fortify.MCA <- function(model, data, ...) {
   ## model doit être un objet de classe MCA
   
@@ -18,14 +34,16 @@ fortify.MCA <- function(model, data, ...) {
     df[(k+1):(k+variables[i]),"label"] <- modalites[[i]]
     k <- k + variables[i]
   }
-  df[,paste("size",1:acm$call$ncp,sep="")] <- acm$var$contrib
+  df[,paste("size",1:model$call$ncp,sep="")] <- model$var$contrib
   df$type <- "variable"
   
   individus <- as.data.frame(cbind(model$ind$coord, model$ind$contrib))
   names(individus) <- c(paste(dimnames(model$var$coord)[[2]],".coord",sep=""),paste(dimnames(model$var$contrib)[[2]],".contrib",sep=""))
   individus[,c("var", "label")] <- NA
-  individus[,paste("size",1:acm$call$ncp,sep="")] <- acm$call$row.w
+  individus[,paste("size",1:model$call$ncp,sep="")] <- model$call$row.w
   individus$type <- "individu"
   df <- rbind(df, individus)
   return(df)
 }
+
+fortify.MCAlist <- fortify.MCA
