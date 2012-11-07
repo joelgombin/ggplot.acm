b0ff2df649175be0a60f67450d1b4802170b5a15
Joel Gombin <joel.gombin@gmail.com>
2012-11-07 17:42:44 +0100

    Commit initial. Remplace ggplot.acm. Peut désormais représenter plus de six variables.

---
+++ R/autoplot.MCA.R
@@ -0,0 +1,89 @@
+autoplot.MCA <- function(object, axes=c(1,2), mod=TRUE, ind=FALSE, filtre=0, axis.plot=TRUE, alpha=1, point.type="petit", ellipses=NA, coloriage=NA, taille=FALSE) {
+  ## object doit être un objet de classe MCA
+  ## axes permet de choisir les dimensions représentées
+  ## mod indique s'il faut représenter les modalités des variables
+  ## ind indique s'il faut représenter les individus (en transparence)
+  ## filtre indique la valeur de la contribution au dessus de laquelle les modalités doivent être représentées. S'il prend la valeur "moyenne", seules les contributions supérieures à la moyenne sont retenues
+  ## axis.plot indique s'il faut tracer les axes
+  ## alpha permet de contrôler l'intensité des points des individus (valeur nulle ou positive)
+  ## point.type contrôle la taille du point des individus. Peut prendre les valeurs "petit" ou "gros"
+  ## ellipses doit être un nom de variable à représenter sous forme d'ellipses
+  ## coloriage doit être un nom de variable, les points sont coloriés en fonction de ses modalités. Dans ce cas, les variables elles-mêmes sont représentées en noir.
+  ## taille doit être une valeur logique,  indiquant si les points doivent être proportionnels à leur poids (contribution pour les modalités) 
+  
+  .e <- environment()
+  toLoad <- c("ggplot2", "directlabels", "rgrs", "boot", "ellipse")
+  erreur <- unlist(lapply(toLoad, require, character.only = TRUE))
+  toInstall <- toLoad[which(erreur %in% FALSE)]
+  if (length(toInstall) > 0) {
+    install.packages(toInstall, repos = "http://cran.r-project.org")
+    lapply(toInstall, require, character.only = TRUE)
+  }
+  
+  df <- fortify(object)
+  df[df$type %in% "variable", "size"] <- df[df$type %in% "variable",sprintf("size%s", axes[1])] + df[df$type %in% "variable",sprintf("size%s", axes[2])]
+  df[df$type %in% "individu", "size"] <- (df[df$type %in% "individu",sprintf("size%s", axes[1])] + df[df$type %in% "individu",sprintf("size%s", axes[2])]) / mean((df[df$type %in% "individu",sprintf("size%s", axes[1])] + df[df$type %in% "individu",sprintf("size%s", axes[2])])) * 4
+  
+  if (filtre %in% "moyenne") {filtre <- 100/dim(df[df$type %in% "variable",])[1]}
+  
+  pt <- "."
+  if (point.type %in% "gros") pt <- 16
+  
+  p <- ggplot(df[(df[df$type %in% "variable",paste("Dim ",axes[1],".contrib",sep="")] > filtre) | (df[df$type %in% "variable",paste("Dim ",axes[2],".contrib",sep="")] > filtre), ], aes(x=get(eval(names(df)[axes[1]])), y=get(eval(names(df)[axes[2]]))),environment=.e) # la base
+  variable <- df[(df[df$type %in% "variable",paste("Dim ",axes[1],".contrib",sep="")] > filtre) | (df[df$type %in% "variable",paste("Dim ",axes[2],".contrib",sep="")] > filtre), "var"]
+  p <- p + xlab(paste("Dimension ",axes[1]," - ",round(object$eig[axes[1],"percentage of variance"],2)," %",sep="")) # légende de l'axe des abcisses
+  p <- p + ylab(paste("Dimension", axes[2]," - ",round(object$eig[axes[2],"percentage of variance"],2)," %",sep="")) # légende de l'axe des ordonnées
+  
+  if (axis.plot) {p <- p + geom_vline(xintercept=0, colour="dark gray") + geom_hline(yintercept=0, colour="dark gray")} # représenter les axes
+  
+  if (ind & is.na(coloriage)) { # représentation des individus sans coloriage
+    p <- p + geom_point(data=df[df$type %in% "individu",],aes(x=get(names(df)[axes[1]]), y=get(names(df)[axes[2]])), shape=pt, alpha=min(1,alpha*1000/dim(df[df$type %in% "individu",])[1]), environment=.e) + scale_shape_manual(values=1:length(unique(pt)))
+    } 
+  
+  if (ind & !(is.na(coloriage)) ) { # représentation des individus avec coloriage
+    data2 <- as.data.frame(cbind(df[df$type %in% "individu",],as.data.frame(object$call$X[,coloriage])))
+    names(data2) <- c(names(df),coloriage)
+p <- p + geom_point(data=data2,aes(x=get(eval(names(data2)[axes[1]])), y=get(eval(names(data2)[axes[2]])), colour=get(eval(coloriage))), shape=pt, alpha=min(1,alpha*1000/dim(df[df$type %in% "individu",])[1])) + scale_colour_discrete(name=coloriage) + guides(colour = guide_legend(override.aes = list(alpha = 1))) + scale_shape_manual(values=1:length(unique(pt)))
+  }
+  
+  if (ind & !(is.na(ellipses))) { ## pour représenter les individus et des ellipses de concentration
+    ## code emprunté à http://stackoverflow.com/questions/2397097/how-can-a-data-ellipse-be-superimposed-on-a-ggplot2-scatterplot
+    data.ellipses <- data.frame(df[df$type %in% "individu",],object$call$X[,ellipses])
+    names(data.ellipses)[(dim(df[df$type %in% "individu",])[2]+1):(dim(df[df$type %in% "individu",])[2]+length(ellipses))] <- ellipses
+    df_ell <- data.frame()
+    for (g in levels(data.ellipses[,ellipses])) {
+      temp <- data.ellipses[data.ellipses[,ellipses]==g,]
+      df_ell <- rbind(df_ell, cbind(as.data.frame(ellipse(corr(as.matrix(data.frame(temp[,names(data.ellipses)[axes[1]]],temp[,names(data.ellipses)[axes[2]]])), w=object$call$row.w[data.ellipses[,ellipses]==g]), 
+                                                          scale=c(sqrt(wtd.var(temp[,names(data.ellipses)[axes[1]]],weights=object$call$row.w[data.ellipses[,ellipses]==g])),sqrt(wtd.var(temp[,names(data.ellipses)[axes[2]]],weights=object$call$row.w[data.ellipses[,ellipses]==g]))), 
+                                                          centre=c(weighted.mean(temp[,names(data.ellipses)[axes[1]]],w=object$call$row.w[data.ellipses[,ellipses]==g]), weighted.mean(temp[,names(data.ellipses)[axes[2]]],w=object$call$row.w[data.ellipses[,ellipses]==g])))),group=g))
+    }
+    if (!(is.na(coloriage)) & (coloriage == ellipses)){
+      p <- p + geom_path(data=df_ell,aes(x=x, y=y,linetype=group,colour=group)) + scale_linetype_discrete(name = ellipses)
+    } else {
+      p <- p + geom_path(data=df_ell,aes(x=x, y=y,linetype=group)) + scale_linetype_discrete(name = ellipses)
+    }
+  }
+  
+  if (mod & is.na(coloriage)) {
+    if (taille) {
+      p <- p + geom_point(aes(colour=var, shape=var, size=size)) + geom_dl(aes(label=label,colour=var, size=size), method="smart.grid", show_guide=FALSE) + scale_colour_discrete(name = "Variables") + scale_shape_manual(name = "Variables",values=1:length(unique(variable))) + scale_size_continuous(guide=FALSE)
+      }
+    else {
+      p <- p + geom_point(aes(colour=var, shape=var), size=4) + geom_dl(aes(label=label,colour=var), method="smart.grid", show_guide=FALSE) + scale_colour_discrete(name = "Variables") + scale_shape_manual(name = "Variables",values=1:length(unique(variable)))
+    }
+  }
+  
+  if (mod & !(is.na(coloriage))) {
+    if (taille) {
+      p <- p + geom_point(aes(shape=var,size=size)) + geom_dl(aes(label=label), method="smart.grid", show_guide=FALSE) + scale_shape_manual(name = "Variables",values=1:length(unique(var))) + scale_size_continuous(guide=FALSE)
+    }
+    else {
+      p <- p + geom_point(aes(shape=var), size=4) + geom_dl(aes(label=label), method="smart.grid", show_guide=FALSE) + scale_shape_manual(name = "Variables",values=1:length(unique(variable)))
+    }
+  }
+  
+  # print(p)
+  return(p)
+}
+
+autoplot.MCAlist <- autoplot.MCA
